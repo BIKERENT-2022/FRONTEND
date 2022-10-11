76b50ffbdb75fe5ade3c2c93165693e7d5cdbe1f
Deysi <u202010511@upc.edu.pe>
2022-10-11 13:23:33 -0500

    tb3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { InicioAppWeb } from './Resources/InicioSesion/InicioSesion.html';
+
+import { RegistroComponent } from './components/registro/registro.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { SolicitudesComponent } from './components/solicitudes/solicitudes.component';
+import { AceptarSolicitudComponent } from './components/aceptar-solicitud/aceptar-solicitud.component';
+import { ClientesPremiumComponent } from './components/clientes-premium/clientes-premium.component';
+import { PerfilPremiumComponent } from './components/perfil-premium/perfil-premium.component';
+import { HomeComponent } from './components/home/home.component';
+import { BicicletasComponent } from './components/bicicletas/bicicletas.component';
 
 const routes: Routes = [
-{path:"", component:}
 
+  
+  {path:"login",component:LoginComponent},
+  {path:"Home",component:HomeComponent},
+  {path:"ClientesPremium",component:ClientesPremiumComponent},
+  {path:"AceptarSolicitud",component:AceptarSolicitudComponent},
+  {path:"PerfilPremium",component:PerfilPremiumComponent},
+  {path:"Solicitudes",component:SolicitudesComponent},
+  {path:"Bicicletas",component:BicicletasComponent},
+  {path:"Registrate",component:RegistroComponent},
+  {path:"CambiarContrasenia",component:BicicletasComponent},
+  {path:"Historial",component:BicicletasComponent},
+  {path:"",component:LoginComponent},
 
 ];
 
---
+++ src/app/components/solicitud-tarjeta/solicitud-tarjeta.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-solicitud-tarjeta',
+  templateUrl: './solicitud-tarjeta.component.html',
+  styleUrls: ['./solicitud-tarjeta.component.css']
+})
+export class SolicitudTarjetaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
