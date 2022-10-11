a316ddbce23fe0e77505165ff0920d39aac3c2a6
Deysi <u202010511@upc.edu.pe>
2022-10-11 13:01:21 -0500

    new

---
+++ src/app/components/pago-basic/pago-basic.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pago-basic',
+  templateUrl: './pago-basic.component.html',
+  styleUrls: ['./pago-basic.component.css']
+})
+export class PagoBasicComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
