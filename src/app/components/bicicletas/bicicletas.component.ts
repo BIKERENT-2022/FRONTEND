6887feef13e7c0a19214cad4c30d754ab32e8cc3
Sunndess <u202010511@upc.edu.pe>
2022-11-24 22:58:15 -0500

    final

--- src/app/components/bicicletas/bicicletas.component.ts
+++ src/app/components/bicicletas/bicicletas.component.ts
@@ -1,4 +1,9 @@
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Bicicleta } from 'src/app/models/Bicicleta';
+import { BicicletaService } from 'src/app/services/bicicleta.service';
 
 @Component({
   selector: 'app-bicicletas',
@@ -7,9 +12,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BicicletasComponent implements OnInit {
 
-  constructor() { }
+  bicicleta: Bicicleta[] = [];
+  dataSource = new MatTableDataSource<Bicicleta>();
+  displayColumns: string[] = ["img"];
+  public MyForm!: FormGroup
+  idCliente!: any;
+
+  constructor(private bicicletaService:BicicletaService,
+    private router: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getBicicletas();
+    this.idCliente = this.route.snapshot.params['idCliente'];
   }
 
+  getBicicletas(): void {
+    this.bicicletaService.getBicicletas().subscribe(
+      (data: Bicicleta[]) => {
+        this.dataSource = new MatTableDataSource(data);
+      }
+    );
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  bicicletaEscogida(id: any){
+    this.router.navigate(["/minibmxx",this.idCliente,id]);
+  }
 }
--- src/app/components/clientes-premium/clientes-premium.component.ts
+++ src/app/components/clientes-premium/clientes-premium.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { ClienteService } from './../../services/cliente.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { Cliente } from './../../models/Cliente';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-clientes-premium',
@@ -7,9 +11,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientesPremiumComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ["id","nombre","dni","correo","direccion","telefono"];
+  dataSource = new MatTableDataSource<Cliente>();
+  cliente: Cliente[] = [];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  
+  constructor(private ClienteService:ClienteService) { }
 
   ngOnInit(): void {
+    this.getListCliente();
+  }
+
+  getListCliente(){
+    this.ClienteService.getCliente().subscribe(
+      (data:Cliente[])=>{
+        console.log(data);
+      this.dataSource = new MatTableDataSource(data);
+      this.dataSource.paginator = this.paginator;
+    })
+  }
+
+
+
+
+  exportExcel() {
+    this.ClienteService.exportCliente().subscribe({
+        next: (data: any) => {
+          let file = new Blob([data], {
+            type:"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+          });
+          let fileURL = URL.createObjectURL(file);
+          var anchor = document.createElement('a');
+          anchor.download="cliente_reporte.xlsx";
+          anchor.href=fileURL;
+          anchor.click();
+        },
+        error: (error: any) => {
+          console.log("No se pudo exportar");
+        }
+      }
+    );
   }
 
 }
